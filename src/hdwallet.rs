99bf5b9fdd983f8699fcc35229794bc2dc41d606
Alexander Cyon <alex.cyon@gmail.com>
2025-02-09 20:03:55 +0100

    vis

--- src/hdwallet.rs
+++ src/hdwallet.rs
@@ -12,19 +12,16 @@ use radix_common::prelude::{
     AddressBech32Encoder, ComponentAddress, NetworkDefinition, Secp256k1PublicKey,
 };
 
-pub const BASE_PATH: &str = "m/44'/1022'/0'/0";
+pub(crate) const BASE_PATH: &str = "m/44'/1022'/0'/0";
 
 pub struct HDWallet {
-    pub entropy: U256,
-    pub mnemonic: Mnemonic,
-    pub intermediary_key_priv_hdwallet: ExtendedPrivKey,
-    pub key_chain: DefaultKeyChain,
+    intermediary_key_priv_hdwallet: ExtendedPrivKey,
     pub mnemonic_phrase: String,
-    pub finger_print: String,
+    finger_print: String,
 }
 
 impl HDWallet {
-    fn new(entropy: U256, mnemonic: Mnemonic) -> Result<Self, RunError> {
+    fn new(mnemonic: Mnemonic) -> Result<Self, RunError> {
         let seed_bytes = mnemonic.to_seed("");
         let seed_fingerprint = general_purpose::STANDARD_NO_PAD.encode(&seed_bytes[56..]);
         let key_chain =
@@ -35,29 +32,24 @@ impl HDWallet {
             .expect("fetch key");
 
         Ok(Self {
-            entropy,
-            mnemonic: mnemonic.clone(),
-            key_chain,
             intermediary_key_priv_hdwallet,
             mnemonic_phrase: mnemonic.to_string(),
             finger_print: seed_fingerprint,
         })
     }
 
+    #[allow(dead_code)] // used by integration tests
     pub fn from_mnemonic_phrase(mnemonic_phrase: &str) -> Result<Self, RunError> {
         let mnemonic =
             Mnemonic::parse(mnemonic_phrase).map_err(|_| RunError::MnemonicFromPhrase)?;
-        let entropy_bytes = mnemonic.to_entropy();
-        let entropy = U256::from_big_endian(entropy_bytes.as_slice());
-        Self::new(entropy, mnemonic)
+        Self::new(mnemonic)
     }
 
     pub fn from_entropy(entropy: U256) -> Result<Self, RunError> {
         let mnemonic = mnemonic_from_u256(&entropy, &Bip39WordCount::Twelve);
-        Self::new(entropy, mnemonic)
+        Self::new(mnemonic)
     }
 }
-
 
 impl HDWallet {
     fn public_key(&self, index: u32) -> Vec<u8> {
@@ -84,13 +76,12 @@ impl HDWallet {
     }
 }
 
-
 #[derive(Clone)]
 pub struct ChildKey {
     pub index: u32,
-    pub public_key_bytes: Vec<u8>,
-    pub address: String,
-    pub suffix: String,
+    public_key_bytes: Vec<u8>,
+    address: String,
+    pub(crate) suffix: String,
 }
 
 fn address_from_public_key(slice: &[u8]) -> String {
@@ -102,7 +93,11 @@ fn address_from_public_key(slice: &[u8]) -> String {
         .expect("bech32 account address")
 }
 
-pub fn vanity_from_childkey(child_key: &ChildKey, target: &str, wallet: &HDWallet) -> Vanity {
+pub(crate) fn vanity_from_childkey(
+    child_key: &ChildKey,
+    target: &str,
+    wallet: &HDWallet,
+) -> Vanity {
     Vanity {
         target: target.to_string(),
         address: child_key.address.clone(),
@@ -111,5 +106,45 @@ pub fn vanity_from_childkey(child_key: &ChildKey, target: &str, wallet: &HDWalle
         public_key_bytes: child_key.public_key_bytes.clone(),
         mnemonic: wallet.mnemonic_phrase.clone(),
         bip39_seed_fingerprint: wallet.finger_print.clone(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn entropy() {
+        let wallet = HDWallet::from_entropy(U256::MAX);
+        assert_eq!(wallet.unwrap().mnemonic_phrase, "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong");
+    }
+
+    #[test]
+    fn mnemonic() {
+        let wallet = HDWallet::from_entropy(U256::zero());
+        assert_eq!(wallet.unwrap().mnemonic_phrase, "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about");
+    }
+
+    #[test]
+    fn test_key() {
+        let wallet = HDWallet::from_mnemonic_phrase(
+            "gentle hawk winner rain embrace erosion call update photo frost fatal wrestle",
+        )
+        .unwrap();
+        let key0 = wallet.derive_child(0);
+        assert_eq!(key0.index, 0);
+        // https://github.com/radixdlt/babylon-wallet-ios/blob/40c7b8d671611ca7a8ba52e0b5e82044d9cebd68/RadixWalletTests/ProfileTests/TestVectors/ProfileVersion100/multi_profile_snapshots_test_version_100.json#L494
+        assert_eq!(
+            hex::encode(key0.public_key_bytes),
+            "02f669a43024d90fde69351ccc53022c2f86708d9b3c42693640733c5778235da5"
+        );
+
+        let key1 = wallet.derive_child(1);
+        assert_eq!(key1.index, 1);
+        // https://github.com/radixdlt/babylon-wallet-ios/blob/40c7b8d671611ca7a8ba52e0b5e82044d9cebd68/RadixWalletTests/ProfileTests/TestVectors/ProfileVersion100/multi_profile_snapshots_test_version_100.json#L542
+        assert_eq!(
+            hex::encode(key1.public_key_bytes),
+            "023a41f437972033fa83c3c4df08dc7d68212ccac07396a29aca971ad5ba3c27c8"
+        );
     }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -3,11 +3,9 @@ mod tests {
 
     use std::collections::HashSet;
 
-    use primitive_types::U256;
     use rad::{
         error::TargetSuffixError,
         find_par::par_find,
-        hdwallet::HDWallet,
         input,
         params::{Bip39WordCount, MAX_INDEX},
         run_config::RunConfig,
@@ -89,41 +87,6 @@ mod tests {
                 "bio".to_string(),
                 'b'
             ))
-        );
-    }
-
-    #[test]
-    fn entropy() {
-        let wallet = HDWallet::from_entropy(U256::one());
-        assert_eq!(wallet.unwrap().entropy, U256::one());
-    }
-
-    #[test]
-    fn mnemonic() {
-        let wallet = HDWallet::from_entropy(U256::zero());
-        assert_eq!(wallet.unwrap().mnemonic_phrase, "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about");
-    }
-
-    #[test]
-    fn test_key() {
-        let wallet = HDWallet::from_mnemonic_phrase(
-            "gentle hawk winner rain embrace erosion call update photo frost fatal wrestle",
-        )
-        .unwrap();
-        let key0 = wallet.derive_child(0);
-        assert_eq!(key0.index, 0);
-        // https://github.com/radixdlt/babylon-wallet-ios/blob/40c7b8d671611ca7a8ba52e0b5e82044d9cebd68/RadixWalletTests/ProfileTests/TestVectors/ProfileVersion100/multi_profile_snapshots_test_version_100.json#L494
-        assert_eq!(
-            hex::encode(key0.public_key_bytes),
-            "02f669a43024d90fde69351ccc53022c2f86708d9b3c42693640733c5778235da5"
-        );
-
-        let key1 = wallet.derive_child(1);
-        assert_eq!(key1.index, 1);
-        // https://github.com/radixdlt/babylon-wallet-ios/blob/40c7b8d671611ca7a8ba52e0b5e82044d9cebd68/RadixWalletTests/ProfileTests/TestVectors/ProfileVersion100/multi_profile_snapshots_test_version_100.json#L542
-        assert_eq!(
-            hex::encode(key1.public_key_bytes),
-            "023a41f437972033fa83c3c4df08dc7d68212ccac07396a29aca971ad5ba3c27c8"
         );
     }
 
